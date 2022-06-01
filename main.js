f07dfe9ba051ec06b8602c040b678defe2e57124
Jihoon Chae <hoonn94@naver.com>
2022-06-01 18:23:33 +0900

    변수설정

---
+++ main.js
@@ -0,0 +1,14 @@
+// 변수 설정
+const vendingMachine = document.querySelector(".vending-machine");
+const Items = vendingMachine.querySelectorAll("li");
+const contBuy = vendingMachine.querySelector(".container-buy");
+const btnReturn = vendingMachine.querySelector(".button-return");
+const numBalance = contBuy.querySelector('.number-balance')
+const inputDeposit = contBuy.querySelector(".input-put");
+const btnPut = contBuy.querySelector(".button-put");
+const btnGet = contBuy.querySelector(".button-get");
+
+const containerItems = document.querySelector('.container-items')
+const listItemsScreen = containerItems.querySelector('.list-items-screen')
+const myInfo = document.querySelector('.my-info')
+const numMyMoney = myInfo.querySelector('.number-mymoney')
